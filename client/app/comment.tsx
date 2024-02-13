3026798f9295e69a7d6119fa844bec7354bd3a78
obranen <krakhnytskyi@gmail.com>
2024-02-13 02:04:38 +0200

    added comment-form

--- client/app/comment.tsx
+++ client/app/comment.tsx
@@ -1,6 +1,10 @@
 import { useQuery } from '@tanstack/react-query'
-import { FlatList, Text, View } from 'react-native'
-import { widthPercentageToDP as wp } from 'react-native-responsive-screen'
+import { FlatList, View } from 'react-native'
+import { Button, Card, TextInput, Text } from 'react-native-paper'
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from 'react-native-responsive-screen'
 import { getUsers } from '../fetch/comment'
 
 export default function TodoScreen() {
@@ -27,16 +31,60 @@ export default function TodoScreen() {
 
   return (
     <View className='flex-1 dark:bg-gray-800'>
+      <Text
+        style={{ fontSize: hp(2.5) }}
+        className='text-center mt-[20px] mb-[6px] font-medium text-fuchsia-600'
+      >
+        Создать комментарий
+      </Text>
+      <TextInput
+        mode='outlined'
+        label='Name'
+        placeholder='Name...'
+        right={<TextInput.Affix />}
+        style={{ width: wp('80%') }}
+        className='mx-auto'
+      />
+      <TextInput
+        mode='outlined'
+        label='Description'
+        placeholder='Description...'
+        right={<TextInput.Affix />}
+        style={{ width: wp('80%') }}
+        className='mx-auto'
+      />
+      <Button
+        icon='camera'
+        mode='contained'
+        onPress={() => console.log('Pressed')}
+        style={{ width: wp('80%') }}
+        className='mx-auto mt-[10px]'
+      >
+        <Text style={{ fontSize: hp(2) }} className='font-bold text-white'>
+          Добавить
+        </Text>
+      </Button>
+      <Text
+        style={{ fontSize: hp(2.2) }}
+        className='text-center mt-[20px] mb-[6px] font-medium text-fuchsia-600'
+      >
+        Список комментариев:
+      </Text>
       <FlatList
         data={comments.data}
         renderItem={({ item }) => (
-          <View
-            style={{ width: wp(80) }}
-            className='p-2 mx-auto my-2 border-2 rounded-md dark:border-white'
-          >
-            <Text className='dark:text-white'>{item.name}</Text>
-            <Text className='dark:text-white'>{item.description}</Text>
-          </View>
+          <Card style={{ width: wp('80%') }} className='mx-auto mb-[10px]'>
+            <Card.Title title={item.name} />
+            {/* <Text
+              style={{ fontSize: hp(2.2) }}
+              className='text-center mt-[20px] mb-[6px] font-medium text-fuchsia-600'
+            >
+              {item.createdDate}
+            </Text> */}
+            <Card.Content>
+              <Text variant='bodyMedium'>{item.description}</Text>
+            </Card.Content>
+          </Card>
         )}
       />
     </View>
--- client/fetch/comment.ts
+++ client/fetch/comment.ts
@@ -4,7 +4,7 @@ import { IComment } from '../interface/comment'
 export const getUsers = async () => {
   try {
     const response = await axios.get<IComment[]>(
-      `${process.env.EXPO_PUBLIC_FETCH_URL}/api/comment`
+      `${process.env.EXPO_PUBLIC_FETCH_MOBILE_URL}/api/comment`
     )
     return response.data
   } catch (error) {
