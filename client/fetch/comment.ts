ffd21176f666138361f2cc2795dee1e6217d9bf3
obranen <krakhnytskyi@gmail.com>
2024-02-14 19:34:29 +0200

    update

--- client/fetch/comment.ts
+++ client/fetch/comment.ts
@@ -1,11 +1,15 @@
 import axios from 'axios'
 import { IComment } from '../interface/comment'
+import { Platform } from 'react-native'
+
+const baseUrl =
+  Platform.OS === 'android'
+    ? process.env.EXPO_PUBLIC_FETCH_MOBILE_URL
+    : process.env.EXPO_PUBLIC_FETCH_WEB_URL
 
 export const getUsers = async () => {
   try {
-    const response = await axios.get<IComment[]>(
-      `${process.env.EXPO_PUBLIC_FETCH_MOBILE_URL}/api/comment`
-    )
+    const response = await axios.get<IComment[]>(`${baseUrl}/api/comment`)
     return response.data
   } catch (error) {
     console.error(error)
